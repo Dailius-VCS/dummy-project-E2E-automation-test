097759a9b32e9051abc78d10236398e888aa37c0
Dailius Raščius <96541023+Dailius-VCS@users.noreply.github.com>
2024-06-07 19:32:17 +0300

    Add positive and negative registration tests

--- src/test/java/lt/vcs/project_name/test/app_name/LoginTest.java
+++ src/test/java/lt/vcs/project_name/test/app_name/LoginTest.java
@@ -1,7 +1,6 @@
 package lt.vcs.project_name.test.app_name;
 
 import lt.vcs.project_name.page.app_name.LoginPage;
-import lt.vcs.project_name.page.app_name.RegistrationPage;
 import lt.vcs.project_name.test.TestBase;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,5 +10,33 @@ public class LoginTest extends TestBase {
     @Override
     public void setUp() {
         LoginPage.open("some_url");
+    }
+
+    @Test(groups = {"regression", "smoke"})
+    public void testLoginWithValidData() {
+        LoginPage.login("username", "password");
+        // Add some test steps
+        // Add assertion
+    }
+
+    @Test(groups = {"regression"})
+    public void testLoginWithInvalidUserName() {
+        LoginPage.login("username2", "password");
+        // Add some test steps
+        // Add assertion
+    }
+
+    @Test(groups = {"regression"})
+    public void testLoginWithInvalidPassword() {
+        LoginPage.login("username3", "password3");
+        // Add some test steps
+        // Add assertion
+    }
+
+    @Test(groups = {"regression"})
+    public void testLoginWithInvalidUserNameAndPassword() {
+        LoginPage.login("username4", "password4");
+        // Add some test steps
+        // Add assertion
     }
 }
